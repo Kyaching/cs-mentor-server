cd91ed92b530e32c7346797dd8add380e1db525a
kyaching <kyochingmarma@gmail.com>
2022-10-25 09:54:21 +0600

    installed cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const courses_category = "./data/course_categories.json";
+const courses_category = require("./data/course_categories.json");
 
 app.get("/", (req, res) => {
   res.send("Courses API Running");
